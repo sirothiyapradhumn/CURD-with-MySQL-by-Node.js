c0f88ae0a65272ca12e6b5c87dd6da1431e82870
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-06-26 18:29:12 +0530

    employee get req

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // get the client
 const mysql = require('mysql2');
+const express = require('express');
+const bodyparser = require('body-parser');
+
+var app = express();
+
+app.use(bodyparser.json());
 
 // create the connection to database
 const mysqlConnection = mysql.createConnection({
@@ -10,11 +16,44 @@ const mysqlConnection = mysql.createConnection({
     database: 'employeedb'
   });
 
-  mysqlConnection.connect((err) => {
+
+// connection req   
+mysqlConnection.connect((err) => {
+if(!err){
+    console.log('DB connection succeded');
+}
+else{
+    console.log('DB connection failed \n Error : ' + JSON.stringify(err, undefined, 2));
+}
+});
+
+app.listen(3000, () => console.log('Express server is running at port no : 3000'));
+
+
+// Get all employees
+app.get('/employee', (req, res)=>{
+    mysqlConnection.query('SELECT * FROM employee', (err, rows, fields) =>{
+        if(!err){
+            //console.log(rows[0].EmpID);
+            //console.log(rows);
+            res.send(rows);
+        }
+        else{
+            console.log(err);
+        }
+    })
+}); 
+
+
+// Get an employees
+// /employee/1
+app.get('/employee/:id', (req, res)=>{
+mysqlConnection.query('SELECT * FROM employee WHERE EmpID = ?', [req.params.id], (err, rows, fields) =>{
     if(!err){
-        console.log('DB connection succeded');
+        res.send(rows);
     }
     else{
-        console.log('DB connection failed \n Error : ' + JSON.stringify(err, undefined, 2));
+        console.log(err);
     }
-  });
+})
+});
